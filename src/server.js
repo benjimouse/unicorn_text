c500dd69af53081c2a103f7c15b679f4f92d92d6
Ben Best <ben@benbat.com>
2025-04-18 17:02:18 +0100

    Win the fight with cors

--- src/server.js
+++ src/server.js
@@ -1,15 +1,5 @@
-const express = require('express');
 const app = require('./app');
 const { PORT } = require('./config/config');
-const cors = require('cors');
-
-// Middleware
-app.use(cors()); // Enable CORS for all routes
-app.options('*', cors()); // Preflight requests
-app.use(express.json()); // Parse JSON requests
-
-// Routes
-app.use('/', require('./routes/api'));
 
 app.listen(PORT, () => {
     console.log(`🚀 Server listening on port ${PORT}`);
