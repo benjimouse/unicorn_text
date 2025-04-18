a027411be0bd7ababf4c2c70c7063a92e2327510
Ben Best <ben@benbat.com>
2025-04-18 17:18:55 +0100

    Create a history in firestore and correct some interesting naming decisions

---
+++ src/utils/firebase.js
@@ -0,0 +1,11 @@
+const admin = require('firebase-admin');
+const { PRIVATE_KEY_JSON } = require('../config/config');
+
+// Initialize Firebase Admin SDK once
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(PRIVATE_KEY_JSON),
+  });
+}
+
+module.exports = admin;
