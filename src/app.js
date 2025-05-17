653b02fe9d5f76050e8c802acd8b78938cb20ca3
Ben Best <ben@benbat.com>
2025-05-17 17:05:57 +0100

    Add request busses

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const app = express();
 const textRoutes = require('routes/text');
 const updateRoutes = require('routes/update');
+const busRoutes = require('routes/bus');
+
 
 // Global Middleware
 app.use(cors());            
@@ -11,6 +13,7 @@ app.use(express.json());
 // Routes
 app.use('/text', textRoutes);
 app.use('/update', updateRoutes);
+app.use('/bus', busRoutes);
 
 // Health check
 app.get('/healthz', (req, res) => {
